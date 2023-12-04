ab2f01da795bf11074903c1ca91e0fdcd664c1a3
tayueda <taylorueda916@gmail.com>
2023-12-03 16:25:47 -0800

    Added logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
+/**
+ * 1. Setup button and servo
+ * 2. Fill out each state
+ *     a. StandBy --> Not reading finger print and door is closed
+ *                    switches to reading print if button pressed
+ *     b. Reading_Print --> Read fingerprint and if is valid then switch
+ *                          to open door. If no valid print found in 10 seconds
+ *                          go back to standby
+ *     c. Open_door --> Open door for 5 seconds, after 5 seconds go back to stand by
+*/
 
+/**
+ * How to change the state
+ * 
+ * doorState = DESIREDSTATE
+ * example: doorState = READING_PRINT
+*/
 #include <Adafruit_Fingerprint.h>
+#include <Servo.h>
 
 
 #if (defined(__AVR__) || defined(ESP8266)) && !defined(__AVR_ATmega2560__)
@@ -24,14 +41,22 @@ bool isValidFingerprintID();
 const int BUTTON_PORT = 4;
 const int SERVO_PORT = 5;
 
+const int HANDLE_DOWN_POSITION = 0;
+const int HANDLE_UP_POSITION = 180;
+
+double time_initial = 0;
+
 enum DoorState{READING_PRINT, STANDBY, OPEN_DOOR};
 DoorState doorState = STANDBY;
+Servo myservo; 
 void setup()
 {
   Serial.begin(9600);
   while (!Serial);  // For Yun/Leo/Micro/Zero/...
   delay(100);
   Serial.println("\n\nAdafruit finger detect test");
+  pinMode(BUTTON_PORT, INPUT);
+  myservo.attach(SERVO_PORT);
 
   // set the data rate for the sensor serial port
   finger.begin(57600);
@@ -68,13 +93,42 @@ void loop()                     // run over and over again
 {
   switch (doorState) {
     case STANDBY:
+      if (digitalRead(BUTTON_PORT) == HIGH) {
+        time_initial = millis() / 1000;
+        doorState = READING_PRINT;
+      } else {
+        myservo.write(HANDLE_UP_POSITION);
+      }
+
+
       break;
     case READING_PRINT:
+      if (isValidFingerprintID() == true) {
+        time_initial = millis() / 1000;
+        doorState = OPEN_DOOR;
+      }
+      double time_final = millis() / 1000;
+      
+      double delta_time = time_final - time_initial;
+      if (delta_time > 10) {
+        doorState = STANDBY;
+      }
+
+      
       break;
+
     case OPEN_DOOR:
+      double time_final = millis() / 1000;
+      myservo.write(HANDLE_DOWN_POSITION);
+
+      double delta_time = time_final - time_initial;
+      if (delta_time > 5) {
+        doorState = STANDBY;
+      }
+
       break;
     default:
-      Serial.println(9600);
+      Serial.println("Error State");
   }
   delay(50);            //don't ned to run this at full speed.
 }
